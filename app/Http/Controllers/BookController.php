6d435d2c0feaf55e05af80591ca0fb7e601353db
Ramon Lima <ramonlima08@gmail.com>
2021-11-30 15:18:45 -0300

    feat: Add BookController and routes

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Book;
+use App\Traits\ApiResponser;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class BookController extends Controller
+{
+    use ApiResponser;   
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Return book List
+     * @return Illuminate/Http/Response
+     * 
+     */
+    public function index()
+    {
+        $books = Book::all();
+        return $this->successResponse($books);
+    }
+
+    /**
+     * Create an instace of book
+     * @return Illuminate/Http/Response
+     * 
+     */
+    public function store(Request $request)
+    {
+        $rules = [
+            'name' => 'required|max:255',
+            'gender' => 'required|max:255|in:male,female',
+            'country' => 'required|max:255',
+        ];
+
+        $this->validate($request, $rules);
+
+        $book = Book::create($request->all());
+
+        return $this->successResponse($book, Response::HTTP_CREATED);
+    }
+
+    /**
+     * Return a specific book
+     * @return Illuminate/Http/Response
+     * 
+     */
+    public function show($book)
+    {
+        $book = Book::findOrFail($book);
+
+        return $this->successResponse($book);
+    }
+
+    /**
+     * Update a specific book
+     * @return Illuminate/Http/Response
+     * 
+     */ 
+    public function update(Request $request, $book)
+    {
+        $rules = [
+            'name' => 'required|max:255',
+            'gender' => 'required|max:255|in:male,female',
+            'country' => 'required|max:255',
+        ];
+
+        $this->validate($request, $rules);
+
+        $book = Book::findOrFail($book);
+
+        $book->fill($request->all());
+
+        if($book->isClean()){
+            $this->errorResponse('Ao menos um campo deve ser alterado', Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+        
+        $book->save();
+
+        return $this->successResponse($book);
+    }
+
+    /**
+     * Remove a specific book
+     * @return Illuminate/Http/Response
+     * 
+     */
+    public function destroy($book)
+    {
+        $book = Book::findOrFail($book);
+
+        $book->delete();
+
+        return $this->successResponse($book);
+    }
+
+    //
+}
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,16 @@
 |
 */
 
+
+//Rotas de Livros
+$router->get('/books', 'BookController@index');
+$router->post('/books', 'BookController@store');
+$router->get('/books/{book}', 'BookController@show');
+$router->put('/books/{book}', 'BookController@update');
+$router->patch('/books/{book}', 'BookController@update');
+$router->delete('/books/{book}', 'BookController@destroy');
+
+
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
